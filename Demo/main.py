0a0ab154e03d1c25566c47003e0a481e02d54ecc
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2026-03-16 18:43:30 +0200

    Спробувала налаштувати онлайн сайт і трохи настроїти структуру сайту

--- Demo/main.py
+++ Demo/main.py
@@ -1,11 +1,24 @@
-from fib_lib import fibonacci
+import sys
+import os
+from flask import Flask, render_template, jsonify
 
-def run():
-    print("--- Тестирование локальной библиотеки ---")
-    fib = fibonacci()
-    
-    for i in range(10):
-        print(f"Число {i+1}: {next(fib)}")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from library.fib_lib import fibonacci  
+
+app = Flask(__name__)
+
+fib_gen = fibonacci()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_fib')
+def get_fib():
+    return jsonify({"number": next(fib_gen)})
 
 if __name__ == "__main__":
-    run()
+    app.run(debug=True)
