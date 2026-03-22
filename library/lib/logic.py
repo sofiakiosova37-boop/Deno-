7d121e25a3ecf0f28d7759c20b1867be9053d54f
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2026-03-22 17:41:03 +0200

    Записала функції для знаходження першої та другої астрономічної швидкостей для різних планет. Це має стати основою для втілення мемоізації. Провела тестування, воно показало, що все працює бездоганно

--- library/lib/logic.py
+++ library/lib/logic.py
@@ -1,10 +1,19 @@
-def fibonacci():
-    a, b = 0, 1
-    while True:
-     yield a
-     a, b = b, a + b
+# Підрахунок першої та другої астрономічної швидкості
+import math 
 
-# fib = fibonacci()
+def calculate_v1(mass, radius):
+    G = 6.67430e-11
+    return math.sqrt((G*mass)/radius)
 
-# for _ in range(10):
-#   print(next(fib))
+def calculate_v2(mass, radius):
+    G = 6.67430e-11
+    return math.sqrt((2*G*mass)/radius)
+
+m = float(input("Введіть масу планети (кг): "))
+r = float(input("Введіть радіус планети (м): "))
+
+v1 = calculate_v1(m, r)
+v2 = calculate_v2(m, r)
+
+print(f"Перша космічна швидкість: {v1:.2f} м/с")
+print(f"Друга космічна швидкість: {v2:.2f} м/с")
