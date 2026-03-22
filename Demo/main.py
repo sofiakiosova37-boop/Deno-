50582781a04588b05a4f0006e8e89fa915190f25
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2026-03-22 15:57:28 +0200

    Виправила кнопку, тепер вона працює. Примітка: 1) Всі логіку, переходи, дії додавати обов'язково в пайтон файл. 2) Рядок про дебаг=тру в пайтоні має бути останнім

--- Demo/main.py
+++ Demo/main.py
@@ -45,5 +45,13 @@ def logout():
     return redirect("/")
 
 
+@app.route('/calcus')  
+def calcus():    
+    return render_template('calcus.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
