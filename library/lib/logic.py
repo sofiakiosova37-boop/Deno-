20daeb745cdca5e1aef7a5fbc39667e311bc4147
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2026-03-23 10:10:54 +0200

    Оновила код для тестування мемоізації

--- library/lib/logic.py
+++ library/lib/logic.py
@@ -13,22 +13,30 @@ def calculate_v2(mass, radius):
     time.sleep(2)
     return round(math.sqrt((2*G*mass)/radius), 2)
 
-get_v1 = Memoize(calculate_v1, max_size=20, ttl=180) # Умови за яких результат в рамках часу -180 секуед - 3 хв та максимальний об'єм 20 планет
-get_v2 = Memoize(calculate_v2, max_size=20, ttl=180)
-
 # Тимчасово для тестування
+start = input("Оберіть стратегію (LRU / LFU / custom): ").strip()
+limit = 3 
+
+get_v1 = Memoize(calculate_v1, max_size=limit, ttl=180, strategy=start) # Умови за яких результат в рамках часу -180 секуед - 3 хв та максимальний об'єм 20 планет
+get_v2 = Memoize(calculate_v2, max_size=limit, ttl=180, strategy=start)
+
 while True:
+    print(f"\nПоточний кеш: {list(get_v1.cache.keys())}")
     m = float(input("Введіть масу планети (кг): "))
     r = float(input("Введіть радіус планети (м): "))
 
+    start_time = time.time()
     v1 = get_v1(m, r)
-    v2 = get_v2(m, r)
+    end_time = time.time()
 
-    print(f"Перша космічна швидкість: {v1} м/с")
-    print(f"Друга космічна швидкість: {v2} м/с")
+    # v1 = get_v1(m, r)
+    # v2 = get_v2(m, r)
 
-    v12 = get_v1(m, r)
-    v22 = get_v2(m, r)
+    print(f"Перша космічна швидкість: {v1} м/с (Час виконання: {end_time - start_time:.2f} сек)")
+    print(f"Друга космічна швидкість: {v2} м/с (Час виконання: {end_time - start_time:.2f} сек)")
+
+    # v12 = get_v1(m, r)
+    # v22 = get_v2(m, r)
    
 
 
