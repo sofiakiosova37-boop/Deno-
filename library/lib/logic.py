6efa8d93a17ecbaab79bbe4c0d6032feca74fe1f
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2026-03-27 00:51:10 +0200

    Доробила ттл і виправила деякі помилки в logic, але  місцями все ще не працює

--- library/lib/logic.py
+++ library/lib/logic.py
@@ -1,6 +1,6 @@
 # Підрахунок першої та другої астрономічної швидкості
 import math 
-from memoization import LFU
+# from memoization import LFU
 from memoization import LRU
 import time
 
@@ -16,10 +16,10 @@ def calculate_v2(mass, radius):
 
 # Тимчасово для тестування
 #start = input("Оберіть стратегію (LRU / LFU / custom): ").strip()
-limit = 10
+limit = 5
 
-get_v1 = Memoize(calculate_v1, max_size=limit, ttl=300) # Умови за яких результат в рамках часу -180 секуед - 3 хв та максимальний об'єм 20 планет
-get_v2 = Memoize(calculate_v2, max_size=limit, ttl=300)
+get_v1 = LRU(calculate_v1, max_size=limit, ttl=300) # Умови за яких результат в рамках часу -180 секуед - 3 хв та максимальний об'єм 20 планет
+get_v2 = LRU(calculate_v2, max_size=limit, ttl=300)
 
 while True:
     print(f"\nПоточний кеш:{len(get_v1.cache)}/{limit}")
@@ -27,19 +27,23 @@ while True:
     r = float(input("Введіть радіус планети (м): "))
 
     start_time = time.time()
-    v1 = get_v1(m, r)
-    v2 = get_v2(m, r)
-    duration = time.time() - start_time
+    key = (m, r)
+    v1 = get_v1.get(key)
+    if v1 is None:
+        v1 = calculate_v1(m, r)
+        get_v1.put(key, v1)
 
-    # v1 = get_v1(m, r)
-    # v2 = get_v2(m, r)
+    v2 = get_v2.get(key)
+    if v2 is None:
+        v2 = calculate_v2(m, r)
+        get_v2.put(key, v2)
+
+    duration = time.time() - start_time
 
     print(f"Перша космічна швидкість: {v1} м/с ")
     print(f"Друга космічна швидкість: {v2} м/с ")
     print(f"Час виконання: {duration:.2f} сек")
 
-    # v12 = get_v1(m, r)
-    # v22 = get_v2(m, r)
    
 
 
--- library/lib/memoization.py
+++ library/lib/memoization.py
@@ -5,9 +5,22 @@ class LRU:
 # метод для роботи з кешом. Це конструктор з даними, які я буду надалі використовувати
     def __init__(self, func, max_size=None, ttl=None): # запис методу з параметрами, максимальний розмір кешу(кількість запитів), ttl параметр часу(за замовчеванням обмеження немає)
         self.func = func # Функція, яку ми обгортаємо
-        self.cache = {} # масив/список з кешом, Сховище. Ключи - аргументи функції
+        self.cache = OrderedDict() # масив/список з кешом, Сховище. Ключи - аргументи функції
         self.max_size = max_size # Ліміт на кількість записів у кеші.
         self.ttl = ttl  # ttl — Time To Live, запису в секундах.
+        self.timestamps = {} # time of adding
+
+    def get(self, key):
+        if key not in self.cache:
+            return None
+        if time.time() is not None:
+            if time.time() - self.timestamps[key] > self.ttl:
+                del self.cache[key]
+                del self.timestamps[key]
+                return None
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
     
     def put(self, key, value):
           if key in self.cache:
@@ -16,12 +29,6 @@ class LRU:
           if len(self.cache) > self.max_size:
               self.cache.popitem(last=False)
               
-    def get(self, key):
-        if key not in self.cache:
-            return None
-        else:
-            self.cache.move_to_end(key)
-        return self.cache[key]
     
 class LFU:
     def __init__(self, func, max_size=None, ttl=None):
