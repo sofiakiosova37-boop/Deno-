b211f0fbe6fa6501485bd2158df2e57ec5be7125
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2026-04-01 18:45:00 +0300

    Виправила логіку списка

--- library/lib/observation.py
+++ library/lib/observation.py
@@ -10,10 +10,12 @@ class SpaceObject:
         self.ra = ra   # Сходження (в годинах або градусах)
         self.dec = dec # градуси нахилу
 
-    elements = [
-        sirius = SpaceObject("Sirius", "Star", -1.46, current_time, 6.75, -16.7),
-        jupiter = SpaceObject("Jupiter", "Planet", -2.50, current_time, 18.5, -23.0),
-        aldebaran = ("Aldebaran", "Star", +0.85, current_time, 4.60, +16.5),
-        cassiopeia = ("Cassiopeia", "Constellation", +2.00, current_time, 1.00, +60.0),
-        mars = SpaceObject("Mars", "Planet", -0.50, current_time, 15.2, -18.0),
+current_time = 0
+
+elements = [
+        SpaceObject("Sirius", "Star", -1.46, current_time, 6.75, -16.7),
+        SpaceObject("Jupiter", "Planet", -2.50, current_time, 18.5, -23.0),
+        SpaceObject("Aldebaran", "Star", +0.85, current_time, 4.60, +16.5),
+        SpaceObject("Cassiopeia", "Constellation", +2.00, current_time, 1.00, +60.0),
+        SpaceObject("Mars", "Planet", -0.50, current_time, 15.2, -18.0),
     ]
