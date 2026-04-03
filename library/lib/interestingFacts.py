fda0dde5f63eaaa6a98082b28f299978ab05bf2e
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2026-04-03 15:00:00 +0300

    Реалізація черга, для іншої задачі

---
+++ library/lib/interestingFacts.py
@@ -0,0 +1,9 @@
+class Facts:
+    def __init__(self, name, discription, distance):
+        self.name = name
+        self.discription = discription
+        self.distance = distance
+
+class priority:
+    def __init__(self):
+        self.data = []
