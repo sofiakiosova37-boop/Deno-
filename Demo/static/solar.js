a2fc713bd369ef802a20e6cf824218d0115e5750
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2026-04-03 20:06:24 +0300

    Додаю інформацію про планети для творення 3д моделі

---
+++ Demo/static/solar.js
@@ -0,0 +1,74 @@
+const planetsData = [
+  {
+    name: "Меркурій",
+    size: 0.5,
+    color: 0xaaaaaa,
+    distance: 10, 
+    speed: 0.04,  
+    rotation: 0.02, 
+    info: "Найменша планета і найближча до Сонця."
+  },
+  {
+    name: "Венера",
+    size: 0.9,
+    color: 0xffcc88,
+    distance: 15,
+    speed: 0.015,
+    rotation: 0.01,
+    info: "Друга планета, найгарячіша в системі через парникові гази."
+  },
+  {
+    name: "Земля",
+    size: 1,
+    color: 0x2233ff,
+    distance: 20,
+    speed: 0.01,
+    rotation: 0.03,
+    info: "Єдина відома планета, на якій існує життя."
+  },
+  {
+    name: "Марс",
+    size: 0.8,
+    color: 0xff3300,
+    distance: 25,
+    speed: 0.008,
+    rotation: 0.02,
+    info: "Червона планета, де колись могла бути вода."
+  },
+  {
+    name: "Юпітер",
+    size: 2.5,
+    color: 0xffaa88,
+    distance: 35,
+    speed: 0.004,
+    rotation: 0.04,
+    info: "Найбільша планета, газовий гігант."
+  },
+  {
+    name: "Сатурн",
+    size: 2.0,
+    color: 0xffddaa,
+    distance: 45,
+    speed: 0.002,
+    rotation: 0.03,
+    info: "Відомий своїми величними кільцями з льоду та пилу."
+  },
+  {
+    name: "Уран",
+    size: 1.5,
+    color: 0x66ffff,
+    distance: 55,
+    speed: 0.0015,
+    rotation: 0.02,
+    info: "Крижаний гігант, що обертається 'лежачи на боці'."
+  },
+  {
+    name: "Нептун",
+    size: 1.4,
+    color: 0x3366ff,
+    distance: 65,
+    speed: 0.001,
+    rotation: 0.02,
+    info: "Найвіддаленіша планета з дуже сильними вітрами."
+  }
+];
