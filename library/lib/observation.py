5ed1de5cc3c4409a0a20368cf8b3e770773f27f3
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2026-04-11 19:02:18 +0300

    Змінила місцями функції, щоб вони були в логічному порядку та додала функцію дісплей, видалила все зайве

--- library/lib/observation.py
+++ library/lib/observation.py
@@ -38,22 +38,3 @@ queue.enqueue(SpaceObject("Aldebaran", "Star", +0.85, 3, 4.60, +16.5))
 queue.enqueue(SpaceObject("Cassiopeia", "Constellation", +2.00, 4, 1.00, +60.0))
 queue.enqueue(SpaceObject("Mars", "Planet", -0.50, 5, 15.2, -18.0))
 queue.display()
-
-"""
-best_object = max(elements, key=lambda x: x.priority)
-worst_object = min(elements, key=lambda x: x.priority)
-print(f"Найкращий для спостереження:", best_object.name)
-print(f"Найменш актуальний об'єкт:", worst_object.name)
-
-elements.remove(best_object)
-target = elements.pop(0)
-print(f"Вилучено найстаріший об'єкт (FIFO): {target.name}")
-last_added = elements.pop()
-print(f"Вилучено найновіший об'єкт (LIFO): {last_added.name}")
-
-print("-" * 30)
-print(f"Зараз у черзі залишилося об'єктів: {len(elements)}")
-
-for obj in elements:
-    print(f"- {obj.name} (Тип: {obj.body_type}, Пріоритет: {obj.priority:.2f})")
-    """
