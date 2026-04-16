10e092a27c897922e9bc35cd9b718ac21e4a5730
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2026-04-16 18:00:00 +0300

    One way

--- Demo/iss.py
+++ Demo/iss.py
@@ -1,7 +1,17 @@
 from flask import render_template
+import urllib2
+import json
 
+req = urllib2.Request("http://api.open-notify.org/iss-now.json")
+response = urllib2.urlopen(req)
+obj = json.loads(response.read())
+print(obj['timestamp'])
+print(obj['iss_position']['latitude'], obj['data']['iss_position']['latitude'])
+
+"""
 async def iss_page():
     urls = [
         "http://open-notify.org", # location
         "http://open-notify.org" # people number
     ]
+    """
