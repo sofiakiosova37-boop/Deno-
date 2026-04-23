cd15cb4cb0aeb66475a107e89127d45cc0ccb318
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2026-04-23 20:36:33 +0300

    Додала функцію для оновлення данних з сайту та для паралельної обробки даних з сайту

--- Demo/iss.py
+++ Demo/iss.py
@@ -4,6 +4,14 @@ import httpx
 
 iss_bp = Blueprint('iss_bp', __name__)
 
+async def map_async(coro_func, iterable):
+    tasks = [asyncio.create_task(coro_func(url)) for url in iterable]
+    return await asyncio.gather(*tasks)
+
+async def fetch_data(url):
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url, timeout=2)
+        return resp.json()
 
 @iss_bp.route('/api/iss-live')
 async def iss_live_data():
@@ -11,23 +19,5 @@ async def iss_live_data():
         "http://open-notify.org", 
         "http://open-notify.org"
     ]
-
-
-
-"""async def map_async(coro_func, iterable):
-    tasks = [asyncio.create_task(coro_func(url)) for url in iterable]
-    return await asyncio.gather(*tasks)
-
-async def fetch_data(url):
-    async with httpx.AsyncClient() as client:
-        resp = await client.get(url)
-        return resp.json()
-    
-async def iss_page():
-    urls = [
-        "http://open-notify.org", 
-        "http://open-notify.org"
-    ]
     results = await map_async(fetch_data, urls)
-    return render_template('iss.html', location=results[0], crew=results[1])
-"""
+    return jsonify({"location": results[0], "crew": results[1]})
