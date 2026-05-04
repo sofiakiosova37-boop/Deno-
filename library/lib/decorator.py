cc1b82333c8ea62220a61428db263d005e7c54c2
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2026-05-04 10:34:35 +0300

    llab 9

--- library/lib/decorator.py
+++ library/lib/decorator.py
@@ -8,3 +8,11 @@ from datetime import datetime
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+def log(level="INFO"):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            return process_log(func, args, kwargs)
+        return wrapper
+    return decorator
