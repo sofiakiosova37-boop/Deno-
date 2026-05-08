822c278b8affe1f150c861701458f5b27656a67f
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2026-05-09 01:26:15 +0300

    Class creation

--- library/lib/news.py
+++ library/lib/news.py
@@ -1,5 +1,10 @@
 import csv
 import os
+import time
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 CSV_PATH = os.path.join(BASE_DIR, 'news.csv')
+
+class AstroEventsEmitter:
+    def __init__(self):
+        self._listeners = {}
