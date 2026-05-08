069802d8af3516ce6a4152088e6ff3397d15f49e
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2026-05-09 01:52:34 +0300

    Add + fix

--- library/lib/news.py
+++ library/lib/news.py
@@ -7,7 +7,14 @@ CSV_PATH = os.path.join(BASE_DIR, 'news.csv')
 
 class AstroEventsEmitter:
     def __init__(self):
-        self._listeners = {}
+        self._listeners = []
+    def subscribe(self, listener):
+        self._listeners.append(listener)
+    def unsubscribe(self, listener):
+        self._listeners.remove(listener)
+    def emit(self, data):
+        for listener in self._listeners:
+                listener(data)
 
 def notification(data):
     print(f"Подія: {data['info']}")
@@ -17,5 +24,7 @@ def news(file_path, emitter):
     try:
         with open(file_path, mode='r', encoding='utf-8') as f:
             reader = csv.DictReader(f) 
+            for row in reader:
+                emitter.emit(row)
     except FileNotFoundError:
         print("Error")
