da0f4030427276da4e20cb93a8ae1de1fa78ff7b
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2026-05-13 21:25:19 +0300

    Dependency Injection Proxy

--- library/lib/client.py
+++ library/lib/client.py
@@ -4,3 +4,12 @@ class BaseHttpClient:
     async def request(self, method, url, **kwargs):
         async with httpx.AsyncClient() as client:
             return await client.request(method, url, **kwargs)
+        
+class AuthProxy:
+    def __init__(self, inner_client, auth_strategy):
+        self._client = inner_client 
+        self._strategy = auth_strategy
+
+    async def request(self, method, url, **kwargs):
+        kwargs = self._strategy.apply(kwargs)
+        return await self._client.request(method, url, **kwargs)
